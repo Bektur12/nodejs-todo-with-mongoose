e79bc87c183214b9a3294be124e10c673176ebf8
Bektur12 <kanybekovbektur22@gmail.com>
2023-02-22 14:19:49 +0600

    connect in db mongoose

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const DB_URL =
+  "mongodb+srv://user:bekaseka@nodejs-backend.wr1or3h.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
   res.status(200).json("hello server");
 });
 
-app.listen(PORT, () => {
-  console.log(PORT, "hello");
-});
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log(PORT, "hello"));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
