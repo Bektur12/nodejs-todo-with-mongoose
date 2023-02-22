bd992e6d72b19fd11f15a39909a593744081c70c
Bektur12 <kanybekovbektur22@gmail.com>
2023-02-22 13:30:03 +0600

    started todo with db mongoose

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json())
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+  res.status(200).json("hello server");
+});
+
+app.listen(PORT, () => {
+  console.log(PORT, "hello");
+});
